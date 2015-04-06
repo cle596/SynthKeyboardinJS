799e871916ce39e9458d0050a286e275614aacf1
changhan <changhanlee@gmail.com>
2015-04-06 16:51:18 -0400

    polite colors

--- keys.js
+++ keys.js
@@ -3,13 +3,13 @@ var black = new Array(5);
 for (i=0;i<8;++i){
   white[i] = new Rect(30*i,0,30,200);
   stage.addChild(white[i]);
-  white[i].fill('#ff0040');
+  white[i].fill('#e78c64');
   white[i].stroke('#000000',3);
 }
 for (i=0;i<6;++i){
   black[i] = new Rect(20+30*i,0,20,150);
   stage.addChild(black[i]);
-  black[i].fill('#0047ab');
+  black[i].fill('#638bb5');
   black[i].stroke('#000000',3);
   if (i==1){
     ++i;
