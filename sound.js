9c08f44ea9aa83afc5e89de541cc78de3c6078f0
changhan <changhanlee@gmail.com>
2015-04-07 13:59:05 -0400

    can'\t figure out noteoff api

--- sound.js
+++ sound.js
@@ -1,19 +1,5 @@
-
-
-
-
 $.getScript("bonsai.js",function(){
-  //movie.on('message:ready', function() {
-  	// send a categorized message to the runner context
-    var synth = T("SynthDef").play();
-    //var synth = T("OscGen", {wave:"wavc(fa23d70f)", mul:.25}).play();
-    synth.def = function(opts) {
-      var osc1, osc2, env;
-      osc1 = T("saw", {freq:opts.freq         , mul:0.25});
-      osc2 = T("saw", {freq:opts.freq * 1.6818, mul:0.20});
-      env  = T("linen", {s:450, r:250, lv:0.5}, osc1, osc2);
-      return env.on("ended", opts.doneAction).bang();
-    };
+    var synth = T("OscGen", {wave:"wavc(fa23d70f)", mul:.25}).play();
     var keydict = T("ndict.key");
     var midicps = T("midicps");
     T("keyboard").on("keydown", function(e) {
@@ -32,23 +18,7 @@ $.getScript("bonsai.js",function(){
       var midi = keydict.at(e.keyCode);
       if (midi) {
         var freq = midicps.at(midi);
-        //synth.allNoteOff();
         synth.noteOffWithFreq(freq);
       }
     }).start();
-
-
-
-
-
-  	movie.sendMessage('externalData', {
-  		nodeData: document.getElementById('keys').innerHTML
-  	});
-  	// send just a message to the runner context
-    //console.log(state);
-    /*
-    movie.sendMessage({
-      bonsai: 'kimchi'
-    });*/
-  //});
 });
